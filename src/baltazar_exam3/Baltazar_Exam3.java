93df2794cd37fff7d0ff20dd2410de5b3ffe80da
Student <Student@CLB3Unit33>
2023-10-07 15:25:55 +0800

    Version1

--- src/baltazar_exam3/Baltazar_Exam3.java
+++ src/baltazar_exam3/Baltazar_Exam3.java
@@ -5,17 +5,87 @@
  */
 package baltazar_exam3;
 
-/**
- *
- * @author Student
- */
-public class Baltazar_Exam3 {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+public class Baltazar_Exam3 {
+private class PriorityQueuesArray{
+            
+            int priority;
+            int data;
+          
+       PriorityQueuesArray next;           
+        }
+
+    private PriorityQueuesArray front = null;
+   static int number = 1;
+    public int deletion() throws Exception{
+          if (front == null){
+              throw new Exception("Queue is Empty");
+          } 
+          int temporary = front.data;
+          front = front.next;
+            
+           return temporary; 
+        
+        }
+    private void insertion(int item, int priority){
+            PriorityQueuesArray node = new PriorityQueuesArray();
+            node.data=item;
+            node.priority = priority;
+            node.next = null;
+            
+            
+            if (front == null || priority <= front.priority) {
+                node.next = front;
+                front = node;
+            }
+            else{
+                PriorityQueuesArray temporary = front;
+                while (temporary.next != null && temporary.next.priority <= priority) {                    
+                    temporary = temporary.next;
+                }
+                   node.next = temporary.next;
+                   temporary.next = node;
+               number++;
+               
+               }
+            
+        }
+     
+        public void display() throws Exception{
+            if (front == null){
+              throw new Exception("Queue is Empty");
+          }
+            PriorityQueuesArray temporary = front;
+            
+            System.out.println("Priority \t\t Pages");
+            
+            while(temporary != null){
+              
+                   System.out.println(temporary.priority + "\t\t" + temporary.data);
+                temporary = temporary.next;
+
+            }
+            System.out.println("\n");
+        }
+    public static void main(String[] args) throws Exception {
+        Baltazar_Exam3 list = new Baltazar_Exam3();
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while (loop) {            
+            System.out.println("Enter Pages: ");
+                    int page = scanner.nextInt();
+                    System.out.println("Enter Priority: ");
+                    int posi = scanner.nextInt();
+                      
+                    list.insertion(page, posi);
+                     if(number > 5){
+                         list.deletion();
+                     }
+                    list.display();
+        }
+                
     }
     
 }
